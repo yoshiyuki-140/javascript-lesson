a32c5ea320899de2da22730f0010381e777dc936
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-02-06 11:50:51 +0900

    add : typeof usage

--- src003/typeofUsage.js
+++ src003/typeofUsage.js
@@ -7,4 +7,20 @@ console.log(typeof num);
 let str = 'こんにちは';
 console.log(typeof str)
 
+let flag = true;
+console.log(typeof flag);
+
+let ary = ['JavaScript', 'Ruby', 'Python'];
+console.log(typeof ary);
+
+let obj = {
+  x: 1,
+  y: 2,
+}
+
+console.log(typeof obj);
+
+let dat = new Date();
+console.log(typeof dat);
+
 // END
