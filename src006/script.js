40b4dc349e11e7f2f333844dd6d9a20d46ac234b
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-02-06 22:38:50 +0900

    add : for...in statement

---
+++ src006/script.js
@@ -0,0 +1,14 @@
+// thinking name of python
+
+// for in statement
+
+let data = {
+  apple: 150,
+  orange: 100,
+  banana: 120,
+};
+
+
+for (let key in data) {
+  console.log(`${key} = ${data[key]}`)
+}
