0b555487c7357949d7f0f4b5728bc0f19914596b
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-02-07 09:40:24 +0900

    add : src017

---
+++ src017/script.js
@@ -0,0 +1,23 @@
+// 組み込みオブジェクトにかんして
+
+/*
+- Object
+- Array
+- Map
+- Set
+- String
+- Boolean
+- Number
+- Function
+- Math
+- Date
+- Error
+- JSON
+- Promise
+- Proxy
+ */
+
+let str = 'hello';
+console.log(str.length);
+
+// Tips : JavaScript標準型にnew演算子でインスタンス化するのは避けろ
